59ce1e4c0b8067885a98062c3b1eb3b7e2b78a26
Cristiano Junior da Silva Perogil <cristianojuniorcol@gmail.com>
2023-04-03 20:01:09 -0300

    Adding query params support

--- routers/getUsers.js
+++ routers/getUsers.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const route = express.Router();
-const db = require("../src/db/db.json")
+const db = require("../src/db/db.json");
 
 route.get("/", (req, res) => {
     return res.send(db);
+})
+
+// Filtering Users
+route.get("/filterusers", (req, res) => {
+    const filterMethod = req.query.filterBy;
+    const filter = req.query.filter.toLowerCase();
+    const queryResult = db.filter((items) => items[filterMethod].toLowerCase() === filter);
+    if (!queryResult) {
+        return res.status(404).json({ status: "User Not Found." })
+    }
+    return res.status(200).json(queryResult)
 })
 
 module.exports = route;
